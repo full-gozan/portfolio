0770f83391c5242b8b4bc5c034a4ac00c49f7563
full-gozan <ahmedgozan@yahoo.com>
2019-10-18 00:34:28 +0200

    read from separate file using fetch URL

--- js/script.js
+++ js/script.js
@@ -1,2 +1,33 @@
-const show=document.getElementById('show');
-show.innerHTML=location.hash
+ function  getContent(fragmentID,call) {
+    let request = new XMLHttpRequest();
+    request.onload=function(){
+        console.log(request.responseText);
+        
+        call(request.responseText);
+        
+    };
+    request.open("GET",'./viewer/aboutme.html');
+    //request.open("GET",fragmentID +'.html');
+request.send(null);
+
+   /*  partials={
+        aboutme:'this is aboutme',
+        contact:'this is contact',
+        projects:'this is projects',
+        feedback:'this is feedback'
+        };
+    call(partials[fragments]); */
+}
+window.addEventListener('hashchange',navigate)
+function navigate () {
+
+    const contentDiv =document.getElementById('content');
+    fragmentID=location.hash.substr(1);
+    getContent(fragmentID,(content)=>{
+        contentDiv.innerHTML=content;
+    })}
+
+    navigate(); 
+
+
+    
