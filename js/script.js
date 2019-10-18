e0a93110d9533be83ecb99d8851137af7e3a61a1
full-gozan <ahmedgozan@yahoo.com>
2019-10-18 12:29:37 +0200

    create fetchData to grab external files

--- js/script.js
+++ js/script.js
@@ -1,29 +1,13 @@
- function  getContent(fragmentID,call) {
-    let request = new XMLHttpRequest();
-    request.onload=function(){
-        console.log(request.responseText);
-        
-        call(request.responseText);
-        
-    };
-    request.open("GET",'./viewer/aboutme.html');
-    //request.open("GET",fragmentID +'.html');
-request.send(null);
-
-   /*  partials={
-        aboutme:'this is aboutme',
-        contact:'this is contact',
-        projects:'this is projects',
-        feedback:'this is feedback'
-        };
-    call(partials[fragments]); */
-}
+ 
 window.addEventListener('hashchange',navigate)
 function navigate () {
 
     const contentDiv =document.getElementById('content');
     fragmentID=location.hash.substr(1);
-    getContent(fragmentID,(content)=>{
+    if(!location.hash){
+        fragmentID='aboutme'
+    }
+    fetcgData.getContent(fragmentID,(content)=>{
         contentDiv.innerHTML=content;
     })}
 
