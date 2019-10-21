2fc6b5f411dc42b4618648ce718193ec303934a5
full-gozan <ahmedgozan@yahoo.com>
2019-10-21 23:41:25 +0200

    fix the conflict

--- js/script.js
+++ js/script.js
@@ -1,30 +1,28 @@
-function getContent(fragmentID,call) {
+function getContent(fragmentID, call) {
     let request = new XMLHttpRequest();
-    request.onload=function(){
+    request.onload = function () {
         console.log(request.responseText);
-        
+
         call(request.responseText);
-        
+
     };
     //request.open("GET",'./viewer/'+fragmentID +'.html');
-    request.open("GET",'https://full-gozan.github.io/portfolio/viewer/'+fragmentID+'.html');
-<<<<<<< HEAD
-=======
+    request.open("GET", 'https://full-gozan.github.io/portfolio/viewer/' + fragmentID + '.html');
+    request.send(null);
 
->>>>>>> 958c5d817d8301a4887e76cb012aa0eaa51af851
-request.send(null);
 
-   
 }
-window.addEventListener('hashchange',navigate)
-function navigate () {
+window.addEventListener('hashchange', navigate)
 
-    const contentDiv =document.getElementById('content');
-    fragmentID=location.hash.substr(1);
-     if(!location.hash){
-        fragmentID='aboutme'
+function navigate() {
+
+    const contentDiv = document.getElementById('content');
+    fragmentID = location.hash.substr(1);
+    if (!location.hash) {
+        fragmentID = 'aboutme'
     }
-    getContent(fragmentID,(content)=>{
-        contentDiv.innerHTML=content;
-    })}
-    navigate(); 
+    getContent(fragmentID, (content) => {
+        contentDiv.innerHTML = content;
+    })
+}
+navigate();
